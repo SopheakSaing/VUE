9bccaa5c1260851e2b6893ce672ad4e2e88683d5
Sopheak Saing <sopheak.saing@khalibre.com>
2023-07-03 15:29:02 +0700

    partially add style & get profild pic

--- basic/project/app.js
+++ basic/project/app.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
-  template: '<h1>Hello {{firstName}}</h1>',
   data(){
     return{
       firstName: 'Dano',
+      lastName: 'NobDy',
+      email: 'nobdydano01@gmail.com',
+      gender: 'female',
+      picture: 'https://randomuser.me/api/portraits/men/3.jpg',
     }
+  },
+  methods:{
+    getUser(){
+    console.log(this.firstName)
+    },
   }
 })
 app.mount('#app')
