056eefb8e9bf9716f38cac4a07e459c2ed04a07d
Sopheak Saing <sopheak.saing@khalibre.com>
2023-07-04 15:05:14 +0700

    initially build from scratch

--- basic/project/app.js
+++ basic/project/app.js
@@ -12,12 +12,12 @@ const app = Vue.createApp({
     async getUser(){
       const res  = await fetch('https://randomuser.me/api')
       const{results} = await res.json()
-      console.log(results);
-      this.firstName = 'Danie'
-      this.lastName ='Menie'
-      this.email = 'nobdydane01@gmail.com'
-      this.gender = 'female'
-      this.picture = 'https://randomuser.me/api/portraits/women/3.jpg'
+//      console.log(results);
+      this.firstName = results[0].name.first
+      this.lastName =results[0].name.last
+      this.email = results[0].email
+      this.gender = results[0].gender
+      this.picture = result[0].picture.large
     },
   }
 })
